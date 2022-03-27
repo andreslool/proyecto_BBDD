fe8cc369d77112ffda25fe95e0f34fd8e32294e2
andreslool <andres.gmg1997@gmail.com>
2022-03-26 22:26:09 -0300

    main

--- index.py
+++ index.py
@@ -1,54 +1,58 @@
 from tkinter import ttk
+from tkinter import messagebox
 from tkinter import *
-
 import sqlite3
+
 
 class Product:
     # connection dir property
     db_name = 'database.db'
 
     def __init__(self, window):
-        # Initializations 
+        # Initializations
         self.wind = window
         self.wind.title('Products Application')
 
-        # Creating a Frame Container 
-        frame = LabelFrame(self.wind, text = 'Register new Product')
-        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+        # Creating a Frame Container
+        frame = LabelFrame(self.wind, text='Register new Product')
+        frame.grid(row=0, column=0, columnspan=3, pady=20)
 
         # Name Input
-        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        Label(frame, text='Name: ').grid(row=1, column=0)
         self.name = Entry(frame)
         self.name.focus()
-        self.name.grid(row = 1, column = 1)
+        self.name.grid(row=1, column=1)
 
         # Price Input
-        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        Label(frame, text='Price: ').grid(row=2, column=0)
         self.price = Entry(frame)
-        self.price.grid(row = 2, column = 1)
+        self.price.grid(row=2, column=1)
 
-        # Button Add Product 
-        ttk.Button(frame, text = 'Save Product', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
+        # Button Add Product
+        ttk.Button(frame, text='Save Product', command=self.add_product).grid(
+            row=3, columnspan=2, sticky=W + E)
 
-        # Output Messages 
-        self.message = Label(text = '', fg = 'red')
-        self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+        # Output Messages
+        self.message = Label(text='', fg='red')
+        self.message.grid(row=3, column=0, columnspan=2, sticky=W + E)
 
         # Table
-        self.tree = ttk.Treeview(height = 10, columns = 2)
-        self.tree.grid(row = 4, column = 0, columnspan = 2)
-        self.tree.heading('#0', text = 'Name', anchor = CENTER)
-        self.tree.heading('#1', text = 'Price', anchor = CENTER)
+        self.tree = ttk.Treeview(height=10, columns=2)
+        self.tree.grid(row=4, column=0, columnspan=2)
+        self.tree.heading('#0', text='Name', anchor=CENTER)
+        self.tree.heading('#1', text='Price', anchor=CENTER)
 
         # Buttons
-        ttk.Button(text = 'DELETE', command = self.delete_product).grid(row = 5, column = 0, sticky = W + E)
-        ttk.Button(text = 'EDIT', command = self.edit_product).grid(row = 5, column = 1, sticky = W + E)
+        ttk.Button(text='DELETE', command=self.delete_product).grid(
+            row=5, column=0, sticky=W + E)
+        ttk.Button(text='EDIT', command=self.edit_product).grid(
+            row=5, column=1, sticky=W + E)
 
         # Filling the Rows
         self.get_products()
 
     # Function to Execute Database Querys
-    def run_query(self, query, parameters = ()):
+    def run_query(self, query, parameters=()):
         with sqlite3.connect(self.db_name) as conn:
             cursor = conn.cursor()
             result = cursor.execute(query, parameters)
@@ -57,7 +61,7 @@ class Product:
 
     # Get Products from Database
     def get_products(self):
-        # cleaning Table 
+        # cleaning Table
         records = self.tree.get_children()
         for element in records:
             self.tree.delete(element)
@@ -66,28 +70,31 @@ class Product:
         db_rows = self.run_query(query)
         # filling data
         for row in db_rows:
-            self.tree.insert('', 0, text = row[1], values = row[2])
+            self.tree.insert('', 0, text=row[1], values=row[2])
 
     # User Input Validation
     def validation(self):
         return len(self.name.get()) != 0 and len(self.price.get()) != 0
 
+    # Add a product in BBDD
     def add_product(self):
         if self.validation():
             query = 'INSERT INTO product VALUES(NULL, ?, ?)'
-            parameters =  (self.name.get(), self.price.get())
+            parameters = (self.name.get(), self.price.get())
             self.run_query(query, parameters)
-            self.message['text'] = 'Product {} added Successfully'.format(self.name.get())
+            self.message['text'] = 'Product {} added Successfully'.format(
+                self.name.get())
             self.name.delete(0, END)
             self.price.delete(0, END)
         else:
             self.message['text'] = 'Name and Price is Required'
         self.get_products()
 
+    # Delete a product in BBDD
     def delete_product(self):
         self.message['text'] = ''
         try:
-           self.tree.item(self.tree.selection())['text'][0]
+            self.tree.item(self.tree.selection())['text'][0]
         except IndexError as e:
             self.message['text'] = 'Please select a Record'
             return
@@ -98,6 +105,7 @@ class Product:
         self.message['text'] = 'Record {} deleted Successfully'.format(name)
         self.get_products()
 
+    # Edit a product in BBDD
     def edit_product(self):
         self.message['text'] = ''
         try:
@@ -110,31 +118,36 @@ class Product:
         self.edit_wind = Toplevel()
         self.edit_wind.title = 'Edit Product'
         # Old Name
-        Label(self.edit_wind, text = 'Old Name:').grid(row = 0, column = 1)
-        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = name), state = 'readonly').grid(row = 0, column = 2)
+        Label(self.edit_wind, text='Old Name:').grid(row=0, column=1)
+        Entry(self.edit_wind, textvariable=StringVar(self.edit_wind,
+              value=name), state='readonly').grid(row=0, column=2)
         # New Name
-        Label(self.edit_wind, text = 'New Name:').grid(row = 1, column = 1)
+        Label(self.edit_wind, text='New Name:').grid(row=1, column=1)
         new_name = Entry(self.edit_wind)
-        new_name.grid(row = 1, column = 2)
+        new_name.grid(row=1, column=2)
 
-        # Old Price 
-        Label(self.edit_wind, text = 'Old Price:').grid(row = 2, column = 1)
-        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_price), state = 'readonly').grid(row = 2, column = 2)
+        # Old Price
+        Label(self.edit_wind, text='Old Price:').grid(row=2, column=1)
+        Entry(self.edit_wind, textvariable=StringVar(self.edit_wind,
+              value=old_price), state='readonly').grid(row=2, column=2)
         # New Price
-        Label(self.edit_wind, text = 'New Price:').grid(row = 3, column = 1)
-        new_price= Entry(self.edit_wind)
-        new_price.grid(row = 3, column = 2)
+        Label(self.edit_wind, text='New Price:').grid(row=3, column=1)
+        new_price = Entry(self.edit_wind)
+        new_price.grid(row=3, column=2)
 
-        Button(self.edit_wind, text = 'Update', command = lambda: self.edit_records(new_name.get(), name, new_price.get(), old_price)).grid(row = 4, column = 2, sticky = W)
+        Button(self.edit_wind, text='Update', command=lambda: self.edit_records(
+            new_name.get(), name, new_price.get(), old_price)).grid(row=4, column=2, sticky=W)
         self.edit_wind.mainloop()
 
+    # Edit a record in BBDD
     def edit_records(self, new_name, name, new_price, old_price):
         query = 'UPDATE product SET name = ?, price = ? WHERE name = ? AND price = ?'
-        parameters = (new_name, new_price,name, old_price)
+        parameters = (new_name, new_price, name, old_price)
         self.run_query(query, parameters)
         self.edit_wind.destroy()
         self.message['text'] = 'Record {} updated successfylly'.format(name)
         self.get_products()
+
 
 if __name__ == '__main__':
     window = Tk()
